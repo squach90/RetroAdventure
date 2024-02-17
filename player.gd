d8d8017ad9b421e965a4cdc8b3b08a11f10812c3
squach90 <160175686+squach90@users.noreply.github.com>
2024-02-17 17:20:05 +0100

    +beaucoup de chose (voir la description)
    +La partie principal de la map
    +Les mouvements du personnage
    +Pouvoir courir avec shift
    +Les animations
    +Le village
    +Le cimtière
    +Le chateau (On ne peut pas rentrer)

---
+++ player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+
+@export var speed = 100
+@onready var animated_sprite = $AnimatedSprite2D
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+	if input_direction.x < 0:
+		animated_sprite.play("gauche")
+	elif input_direction.x > 0:
+		animated_sprite.play("droite")
+	elif input_direction.y < 0:
+		animated_sprite.play("droite")
+	elif input_direction.y > 0:
+		animated_sprite.play("droite")
+	else:
+		animated_sprite.play("vide")
+		
+	if Input.is_action_pressed("shift"):
+		speed = 150
+	else:
+		speed = 100
+
+func _physics_process(delta):
+	move_and_slide()
+	get_input()
